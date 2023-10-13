a55cd11915fdcd78f0e27eb4dda53fac42aaf57a
Fidail Shafikov <93327801+Shafiddam@users.noreply.github.com>
2023-10-13 17:30:48 +0300

    Add files via upload

--- conftest.py
+++ conftest.py
@@ -2,16 +2,14 @@ from selenium import webdriver
 import pytest
 from selenium.webdriver.chrome.options import Options  # для запуска браузера в безоконном режиме; на английском
 from selenium.webdriver.chrome.service import Service
-from .pages.login_page import LoginPage
+from .pages.admin_page import AdminPage
 from .data.data import *
 import os
 
 
 @pytest.fixture
 def driver():
-    """
-    Функция для настройки браузера
-    """
+    """ Функция для настройки браузера """
     chrome_options = Options()
     chrome_options.add_argument("--lang=en-US")  # Установите желаемый язык, например, en-US для английского
     # chrome_options.add_argument("--headless")
@@ -24,9 +22,28 @@ def driver():
     driver.quit()
 
 
+@pytest.fixture
+def login_and_prepare():
+    password = passwords.get('kznadmin', "DefaultPasswordIfNotFound")
+    login = list(passwords.keys())[0]
+
+    chrome_options = Options()
+    chrome_options.add_argument("--lang=en-US")
+    # chrome_options.add_argument("--headless")
+    driver_path = "c:\\Chromedriver\\chromedriver.exe"
+    service = Service(driver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.maximize_window()
+    admin_page = AdminPage(driver)
+    yield driver
+    driver.quit()
+
+
 def take_screenshot(driver, screenshot_name, screenshots_folder="screenshots"):
     if not os.path.exists(screenshots_folder):
         os.makedirs(screenshots_folder)
     screenshot_path = os.path.join(screenshots_folder, screenshot_name)
     driver.save_screenshot(screenshot_path)
     print(f"Screenshot saved as {screenshot_path}")
+
+
